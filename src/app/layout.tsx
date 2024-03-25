376f560fd11d055edd6f6e2d7f9aaf298f0fa5e0
abrusu-collins <snillochemingson@gmail.com>
2024-03-25 16:24:33 +0000

    adwuma nasi

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import "../styles/style.css";
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/components/home/hero.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <p className="title">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt,
+        eveniet facilis. Delectus veniet facilis. Delectus
+      </p>
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae omnis quam
+        sapiente reprehenderit hic magni animi molestias quasi neque, nobis enim
+        ratione, consectetur nisi numquam nulla magnam. Accusamus, eum quidem.
+      </p>
+
+      <div className="cta">
+        <a href="">Open</a>
+        <a href="">Close</a>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
