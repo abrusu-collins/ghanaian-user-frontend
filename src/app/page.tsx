c1f6b6f95f89259af2acb35db8c98d446907eb2b
abrusu-collins <snillochemingson@gmail.com>
2024-03-27 10:20:03 +0000

    nice work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Features from "@/components/home/features";
 import Hero from "@/components/home/hero";
+import Playground from "@/components/home/playground";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Features />
+      <Playground />
     </>
   );
 }
---
+++ src/components/home/features.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+function Features() {
+  return (
+    <div className="features">
+      <div className="features-inner">
+        <div className="feature">
+          <div className="emoji">🎯</div>
+          <p className="title">
+            Lorem ipsum, dolor sit amet consectetur adipisicing
+          </p>
+          <p className="text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+            fuga sapiente cupiditate
+          </p>
+        </div>
+        <div className="feature">
+          <div className="emoji">🔥</div>
+          <p className="title">
+            Lorem ipsum, dolor sit amet consectetur adipisicing
+          </p>
+          <p className="text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+            fuga sapiente cupiditate
+          </p>
+        </div>
+        <div className="feature">
+          <div className="emoji">🫡</div>
+          <p className="title">
+            Lorem ipsum, dolor sit amet consectetur adipisicing
+          </p>
+          <p className="text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+            fuga sapiente cupiditate
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Features;
